17988e4c9205a32a1453ae8de1432af2cc9c330e
Jared <lotrfreak95@yahoo.com>
2016-04-04 21:34:09 -0500

    Update Map.h

--- Map.h
+++ Map.h
@@ -1,4 +1,7 @@
-
+//Made a demo header for y'all to start with. We need a background for our game, and Jumpei and I picked this one out. 
+//In the struct or class we'll be building to connect the working parts, we can call this void() funtion to attach
+//the background onto the screen. Widgets and satallites will go over it, so we'll have to call this first to ensure
+//it doesn't cover anything.
 
 void map()
 {
